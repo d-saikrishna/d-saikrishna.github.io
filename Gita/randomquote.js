096cd521a5e490a116a0db5b415131040316c3a1
d-saikrishna <dsk.cyanide70@example.com>
2025-02-07 10:49:24 +0530

    UPDATE Gita - Google Sheets

--- Gita/randomquote.js
+++ Gita/randomquote.js
@@ -9,14 +9,15 @@ function displayRandomQuote(csvData) {
 
     console.log(lines.length)
 
-    document.getElementById("quote-text").textContent = '"'+randomQuote.split(':')[0]+'"';
+    document.getElementById("quote-text").textContent = randomQuote.split(':')[0];
     document.getElementById("author-name").textContent = '― '+randomQuote.split(':')[2];
     document.getElementById("remark-text").textContent = randomQuote.split(':')[1];
   }
 
 // Read and parse the CSV file
+const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRXcTCbuNMr-OxZ4XcpyShPkg1AnAu6Ct3C3Umkf5NAPAY8nm5rqXhleyiY5_oIzFZ5YJUT7KnJYSlk/pub?gid=1385646189&single=true&output=csv";
 function loadquotesCSV() {
-    fetch("Gita.csv")
+    fetch(GOOGLE_SHEET_URL)
       .then(function(response) {
         if (response.ok) {
           return response.text();
@@ -24,7 +25,7 @@ function loadquotesCSV() {
         throw new Error("Error: " + response.status);
       })
       .then(function(csvData) {
-        console.log(csvData)
+        // console.log(csvData)
         displayRandomQuote(csvData);
       })
       .catch(function(error) {
